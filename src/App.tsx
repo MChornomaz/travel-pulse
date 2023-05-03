09bc73255c3fde0a31642cad3dccdf4b456e236a
MChornomaz <chornomaz.maksym@gmail.com>
2023-05-03 21:40:42 +0300

    prepublish

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import React, { useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect, Suspense } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 import Header from './components/Header/Header';
-import MainPage from './pages/MainPage/MainPage';
 import Footer from './components/Footer/Footer';
-import DestinationPage from './pages/Destination/DestinationPage';
-import StoriesPage from './pages/StoriesPage/StoriesPage';
-import ReviewsPage from './pages/ReviewsPage/ReviewsPage';
+import Spinner from './UI/Spinner/Spinner';
 import SinglePost from './pages/SinglePost/SinglePost';
-import Page404 from './pages/Page404/Page404';
 import PlacesPage from './pages/PlacesPage/PlacesPage';
-import SearchResultsPage from './pages/SearchResultsPage/SearchResultsPage';
 import Sidebar from './components/SideBar/SideBar';
 import { useAppDispatch, useAppSelector } from './store/hooks';
 import { selectSearch, selectUser } from './store/store';
@@ -19,11 +14,23 @@ import { ROUTES } from './constants';
 
 import './App.css';
 
+const MainPage = React.lazy(() => import('./pages/MainPage/MainPage'));
+const StoriesPage = React.lazy(() => import('./pages/StoriesPage/StoriesPage'));
+const DestinationPage = React.lazy(() => import('./pages/Destination/DestinationPage'));
+const ReviewsPage = React.lazy(() => import('./pages/ReviewsPage/ReviewsPage'));
+const Page404 = React.lazy(() => import('./pages/Page404/Page404'));
+const SearchResultsPage = React.lazy(() => import('./pages/SearchResultsPage/SearchResultsPage'));
+
 function App() {
     const { searchString } = useAppSelector(selectSearch);
 
     const { isAuth, role } = useAppSelector(selectUser);
+    const { pathname } = useLocation();
     const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
 
     useEffect(() => {
         if (!isAuth) {
@@ -47,25 +54,33 @@ function App() {
         <>
             <Header />
             <Sidebar />
-            <Routes>
-                <Route path="/" element={<MainPage />} />
-                <Route path={ROUTES.DESTINATIONS} element={<DestinationPage />} />
-                <Route path={`${ROUTES.DESTINATIONS}/:postId`} element={<SinglePost />} />
-                <Route path={ROUTES.STORIES} element={<StoriesPage />} />
-                <Route path={`${ROUTES.STORIES}/:postId`} element={<SinglePost />} />
-                <Route path={`${ROUTES.REVIEWS}/`} element={<ReviewsPage />}>
-                    {isAuth && <Route path={ROUTES.ADD_REVIEW} element={<ReviewsPage />} />}
-                    {isAuth && role === 'admin' && (
-                        <Route path={`${ROUTES.UPDATE_REVIEW}/:reviewId`} element={<ReviewsPage />} />
-                    )}
-                </Route>
-                <Route path={`${ROUTES.REVIEWS}/:postId`} element={<SinglePost />} />
-                <Route path={`${ROUTES.INFO}/:postId`} element={<SinglePost />} />
-                <Route path={ROUTES.PLACES} element={<PlacesPage />} />
-                {searchString.trim().length > 0 && <Route path={ROUTES.RESULTS} element={<SearchResultsPage />} />}
-                <Route path={`${ROUTES.PLACES}/:postId`} element={<SinglePost />} />
-                <Route path="*" element={<Page404 />} />
-            </Routes>
+            <Suspense
+                fallback={
+                    <div className="fallback">
+                        <Spinner />
+                    </div>
+                }
+            >
+                <Routes>
+                    <Route path="/" element={<MainPage />} />
+                    <Route path={ROUTES.DESTINATIONS} element={<DestinationPage />} />
+                    <Route path={`${ROUTES.DESTINATIONS}/:postId`} element={<SinglePost />} />
+                    <Route path={ROUTES.STORIES} element={<StoriesPage />} />
+                    <Route path={`${ROUTES.STORIES}/:postId`} element={<SinglePost />} />
+                    <Route path={`${ROUTES.REVIEWS}/`} element={<ReviewsPage />}>
+                        {isAuth && <Route path={ROUTES.ADD_REVIEW} element={<ReviewsPage />} />}
+                        {isAuth && role === 'admin' && (
+                            <Route path={`${ROUTES.UPDATE_REVIEW}/:reviewId`} element={<ReviewsPage />} />
+                        )}
+                    </Route>
+                    <Route path={`${ROUTES.REVIEWS}/:postId`} element={<SinglePost />} />
+                    <Route path={`${ROUTES.INFO}/:postId`} element={<SinglePost />} />
+                    <Route path={ROUTES.PLACES} element={<PlacesPage />} />
+                    {searchString.trim().length > 0 && <Route path={ROUTES.RESULTS} element={<SearchResultsPage />} />}
+                    <Route path={`${ROUTES.PLACES}/:postId`} element={<SinglePost />} />
+                    <Route path="*" element={<Page404 />} />
+                </Routes>
+            </Suspense>
             <Footer />
         </>
     );
